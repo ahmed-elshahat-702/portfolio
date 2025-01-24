20dffea4659455c5ff4ce8af2d9b41668411fa1b
ahmed-elshahat-702 <ahmedelshahat702@gmail.com>
2025-01-24 16:00:23 +0200

    feat: enhance projects page layout with MaxWidthContainer, improve error handling, and add loading state

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,12 +1,16 @@
 "use client";
 
 import LoadingSpinner from "@/components/LoadingSpinner";
+import MaxWidthContainer from "@/components/MaxWidthContainer";
 import ProjectCard from "@/components/ProjectCard";
+import Square from "@/components/Square";
+import { useToast } from "@/hooks/use-toast";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const page = () => {
   const [projects, setProjects] = useState();
+  const toast = useToast();
 
   useEffect(() => {
     async function fetchProjects() {
@@ -14,43 +18,54 @@ const page = () => {
         const res = await axios.get("/api/projects");
         setProjects(res.data);
       } catch (error) {
-        console.error("Error fetching projects:", error);
+        toast({
+          variant: "destructive",
+          title: "Error",
+          message: "Error fetching projects",
+        });
       }
     }
     fetchProjects();
   }, []);
   return (
-    <div className="w-full min-h-full bg-[#e6dace] dark:bg-muted/40 flex flex-col items-center p-8 max-sm:px-4">
-      <div className="w-[700px] mx-auto h-full max-lg:w-5/6 max-sm:w-full flex flex-col gap-20">
-        <div className="heading text-center flex flex-col gap-20 items-center">
-          <div className="flex gap-2 items-center font-bold w-fit pl-4">
-            <div className="w-4 h-4 bg-blue-600"></div>
-            <h1 className="text-4xl">Projects</h1>(
-            {!projects ? (
-              <LoadingSpinner />
-            ) : (
-              <span className="text-3xl">{projects.length}</span>
-            )}
-            )
-          </div>
-          <p>
-            I specialize in front-end development, driven by a passion for
-            creating engaging digital experiences. My skills encompass HTML5,
-            CSS3, JavaScript, and various frameworks like React, Next.js an
-            more.... I focus on mboth the technical and design aspects of web
-            development, ensuring user-centered, visually appealing, and
-            intuitive interfaces. I hope you like it
-          </p>
+    <MaxWidthContainer className={"space-y-12"}>
+      <div className="heading text-center flex flex-col gap-10 items-center">
+        <div className="flex gap-2 items-center justify-center font-bold w-full">
+          <Square />
+          <h1 className="text-4xl">Projects</h1>(
+          {!projects ? (
+            <LoadingSpinner />
+          ) : (
+            <div className="text-3xl flex items-center gap-1">
+              <span className="text-main">"</span>
+              {projects.length}
+              <span className="text-main">"</span>
+            </div>
+          )}
+          )
         </div>
-        {projects ? (
-          projects.map((project, index) => (
-            <ProjectCard key={index} project={project} />
-          ))
-        ) : (
-          <ProjectCard />
-        )}
+        <p className="max-w-3xl">
+          I specialize in front-end development, driven by a passion for
+          creating engaging digital experiences. My skills encompass HTML5,
+          CSS3, JavaScript, and various frameworks like React, Next.js an
+          more.... I focus on mboth the technical and design aspects of web
+          development, ensuring user-centered, visually appealing, and intuitive
+          interfaces. I hope you like it
+        </p>
       </div>
-    </div>
+
+      {projects ? (
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-4 justify-items-center">
+          {projects.map((project, index) => (
+            <ProjectCard key={index} project={project} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col items-center">
+          <ProjectCard />
+        </div>
+      )}
+    </MaxWidthContainer>
   );
 };
 
